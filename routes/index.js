16e072cf69911d3df62f7fe0b6eb641710c176cc
laanak <laanak@gmail.com>
2014-06-23 17:17:58 -0400

    app front-end fleshed out

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.render('homepage', { title: 'Express' });
+});
+
+router.get('/meet', function(req, res) {
+  res.render('picking_occasion', { title: 'Express' });
+});
+
+router.get('/picktimes', function(req, res) {
+  res.render('picking_times', { title: 'Express' });
+});
+
+router.get('/picking_locations', function(req, res) {
+  res.render('picking_locations', { title: 'Express' });
+});
+
+router.get('/confirmation', function(req, res) {
+  res.render('confirmation', { title: 'Express' });
+});
+
+router.post('/signin', function(req, res) {
+  res.render('homepage', { title: 'Express' });
+});
+
+
+
+module.exports = router;
