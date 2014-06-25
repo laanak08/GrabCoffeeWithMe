424b49e8086c43991ac664bff1f0811a53b4d599
Ian Donahue <donahuim@mail.uc.edu>
2014-06-25 16:32:01 -0400

    working out js for the time picker

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,8 +3,10 @@ console.log('\'Allo \'Allo!');
 $(function() {
 	console.log("Document Ready");
   	$(".timeAvailable").click(function(){
-		$(this).addClass("selected");
+		$(this).toggleClass("selected");
 		$(this).text("Available");
+		//$(this).children("i").toggleClass("fa-check-circle-o").toggleClass("fa-circle-o");
+
 
 	});
 
