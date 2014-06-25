7708dc7122666066f97f2ed7c9ec41e7dc54155e
Ian Donahue <donahuim@mail.uc.edu>
2014-06-25 16:11:04 -0400

    basic javascript on times interaction

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,11 @@
 console.log('\'Allo \'Allo!');
+
+$(function() {
+	console.log("Document Ready");
+  	$(".timeAvailable").click(function(){
+		$(this).addClass("selected");
+		$(this).text("Available");
+
+	});
+
+});
