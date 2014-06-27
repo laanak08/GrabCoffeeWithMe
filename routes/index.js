58f8780de0622ae8d527cffa63b4b050e23e7276
Tim <timmyg13@gmail.com>
2014-06-27 09:45:18 -0400

    link and create javascript files for all routes

--- routes/index.js
+++ routes/index.js
@@ -2,39 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('homepage', { title: 'Express', js: 'javascripts/test.js'});
+  res.render('homepage', { title: 'Express', js: 'home.js'});
 });
 
 router.get('/meet', function(req, res) {
-  res.render('picking_occasion', { title: 'Express' });
+  res.render('picking_occasion', { title: 'Express', , js: 'meet.js' });
 });
 
 router.get('/picktimes', function(req, res) {
-  res.render('picking_times', { title: 'Express', url :  '/picktimes'});
+  res.render('picking_times', { title: 'Express', url :  '/picktimes', js: 'picktimes.js'});
 });
 
 router.get('/picking_locations', function(req, res) {
-  res.render('picking_locations', { title: 'Express' });
+  res.render('picking_locations', { title: 'Express', js: 'picking_locations.js' });
 });
 
 router.get('/confirmation', function(req, res) {
-  res.render('confirmation', { title: 'Express' });
+  res.render('confirmation', { title: 'Express', js: 'confirmation.js' });
 });
 
 router.get('/terms', function(req, res) {
-  res.render('terms_of_service', { title: 'Express' });
+  res.render('terms_of_service', { title: 'Express', js: 'terms.js' });
 });
 
 router.get('/finalize', function(req, res) {
-  res.render('meetup_finalized', { title: 'Express' });
+  res.render('meetup_finalized', { title: 'Express', js: 'finalize.js' });
 });
 
 router.post('/signin', function(req, res) {
-  res.render('homepage', { title: 'Express' });
+  res.render('homepage', { title: 'Express', js: 'signin.js' });
 });
 
 router.get('/marketplace', function(req, res) {
-  res.render('marketplace', { title: 'Express' });
+  res.render('marketplace', { title: 'Express', js: 'marketplace.js' });
 });
 
 router.post('/meetup', function(req, res) {
